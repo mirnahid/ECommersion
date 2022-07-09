22a87d4cf9b1fb0205bd97d872e81e8d784d3801
mirnahid <mirnahid@hotmail.com>
2022-07-09 14:02:55 +0400

    Created cutom httpclient

--- ECommersionClient/src/app/admin/components/products/products.component.ts
+++ ECommersionClient/src/app/admin/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -9,12 +10,23 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner:NgxSpinnerService) {
+  constructor(spinner:NgxSpinnerService,private httpClientService:HttpClientService) {
     super(spinner);
    }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallScaleMultiple);
+    this.httpClientService.get({
+      controller:"Products"
+    }).subscribe(data=>console.log(data));
+
+    this.httpClientService.post({
+      controller:"Products"
+    },{
+      name:"test",
+      stock:100,
+      price:10
+    }).subscribe();
   }
 
 }
