68758cb299d2cc6da5fa4141ba7b7a6ba52ae282
mirnahid <mirnahid@hotmail.com>
2022-07-27 23:21:23 +0400

    Fixed bug in delete product service

--- ECommersionClient/src/app/admin/components/products/products.module.ts
+++ ECommersionClient/src/app/admin/components/products/products.module.ts
@@ -12,6 +12,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
 
 
 @NgModule({
@@ -19,7 +21,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     ProductsComponent,
     CreateComponent,
     ListComponent,
-    DeleteDirective
+    DeleteDirective,
+    DeleteDialogComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +34,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule
   ]
 })
 export class ProductsModule { }
