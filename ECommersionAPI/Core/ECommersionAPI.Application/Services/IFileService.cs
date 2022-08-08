be99a76b7a94176a7eca98894ddc5f0dc3ca34de
mirnahid <mirnahid@hotmail.com>
2022-08-08 22:33:04 +0400

    Created file service

---
+++ ECommersionAPI/Core/ECommersionAPI.Application/Services/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ECommersionAPI.Application.Services
+{
+    public interface IFileService
+    {
+        Task<List<(string filename, string path)>> UploadAsync(string path,IFormFileCollection files);
+        Task<string> FileRenameAsync(string fileName);
+        Task<bool> CopyFileAsync(string path,IFormFile file);
+    }
+}
---
+++ ECommersionAPI/Infrastructure/ECommersionAPI.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using ECommersionAPI.Application.Services;
+using ECommersionAPI.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommersionAPI.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureService(this IServiceCollection services)
+        {
+            services.AddScoped<IFileService, FileService>();
+        }
+    }
+}
