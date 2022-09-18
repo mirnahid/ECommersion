d071cad25fcf8b5f3a188c817b19205747bb2efc
mirnahid <mirnahid@hotmail.com>
2022-09-18 20:24:13 +0400

    Customized login pages

--- ECommersionClient/src/app/services/common/model/user.service.ts
+++ ECommersionClient/src/app/services/common/model/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { async, firstValueFrom, Observable } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/tokenReponse';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,7 +12,7 @@ import { HttpClientService } from '../http-client.service';
 })
 export class UserService {
 
-  constructor(private httpClientService:HttpClientService) { }
+  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService) { }
 
  async create(user:User):Promise<Create_User>{
    const observable:Observable<Create_User|User>= this.httpClientService.post<Create_User|User>({
@@ -19,13 +22,21 @@ export class UserService {
    return await firstValueFrom(observable) as Create_User ;
   }
 
-  async Login(userNameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<void>{
-    const observable: Observable<any>= this.httpClientService.post({
+  async Login(userNameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<any>{
+    const observable: Observable<any|TokenResponse>= this.httpClientService.post<any|TokenResponse>({
       controller:"users",
       action:"login"
      },{userNameOrEmail, password});
     
-     await firstValueFrom(observable);
+     const token:TokenResponse= await firstValueFrom(observable) as TokenResponse;
+if(token){
+localStorage.setItem("accessToken",token.token.accessToken);
+this.toastrService.message("login success","success",{
+  messageType:ToastrMessageType.Success,
+  position:ToastrPosition.TopRight
+})
+}
+
      callBackFunction();
 
   }
