0aff3ef4c971908dfc9e8942bb5b18974743b718
mirnahid <mirnahid@hotmail.com>
2022-09-20 23:06:26 +0400

    Added guard service

--- ECommersionClient/src/app/app-routing.module.ts
+++ ECommersionClient/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
+import { AuthGuard } from './guards/common/auth.guard';
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
@@ -14,7 +15,7 @@ const routes: Routes = [
     {path:"orders",loadChildren:()=>import("./admin/components/orders/orders.module").then
     (module=>module.OrdersModule)}
   
-  ]},
+  ],canActivate:[AuthGuard]},
   {path:"",component:HomeComponent},
   {path:"basket",loadChildren:()=>import("./ui/components/baskets/baskets.module").then
   (module=>module.BasketsModule)},
